9dcfec3cf5ce037373583af9265c52ba0bab751a
Bryan Braun <bbraun7@gmail.com>
2020-03-19 01:13:47 -0400

    feat: update radios and checkboxes to be structured for easier styling
    Also: merge the docs site and the repo containing the main markup file.

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+// Function for selecting text. See http://stackoverflow.com/a/987376/1154642.
+function selectText(element) {
+  var doc = document,
+      text = doc.getElementById(element),
+      range,
+      selection;
+  if (doc.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(text);
+      range.select();
+  } else if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+  }
+}
+
+// Add click listeners to our `Select All` buttons.
+var htmlButton = document.getElementById('select-html');
+var mdButton = document.getElementById('select-md');
+var selectHtml = function(){ selectText('html-snippet'); };
+var selectMd = function(){ selectText('markdown-snippet'); };
+htmlButton.addEventListener('click', selectHtml);
+mdButton.addEventListener('click', selectMd);
